74a48f1c6812b70de1656c3082f60c14c9abb455
Emilie <92803520+SakoSuta@users.noreply.github.com>
2023-06-26 04:34:14 +0200

    All Routes

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,5 +1,11 @@
 <script setup>
 import { ref } from 'vue';
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import Dropdown from '@/Components/Dropdown.vue';
+import DropdownLink from '@/Components/DropdownLink.vue';
+import NavLink from '@/Components/NavLink.vue';
+import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+import { Link } from '@inertiajs/vue3';
 
 const showingNavigationDropdown = ref(false);
 </script>
@@ -12,17 +18,19 @@ const showingNavigationDropdown = ref(false);
                     <div class="h-full flex flex-col justify-around">
                         <div class="flex justify-center"><img src="/Logo/Very_long_item.svg" alt="Logo" class="w-36"></div>
                         <ul class="list-none p-0 m-0">
-                            <li class="p-4 flex"><img src="/Icone/user.svg" alt="icone" class="px-6"><a href="#" class="text-white no-underline">User</a></li>
-                            <li class="p-4 flex"><img src="/Icone/Subscription.svg" alt="icone" class="px-6"><a href="#" class="text-white no-underline">Subscriptions</a></li>
-                            <li class="p-4 flex"><img src="/Icone/News.svg" alt="icone" class="px-6"><a href="#" class="text-white no-underline">Post</a></li>
-                            <li class="p-4 flex"><img src="/Icone/Subscription.svg" alt="icone" class="px-6"><a href="#" class="text-white no-underline">Plan</a></li>
-                            <li class="p-4 flex"><img src="/Icone/Discount.svg" alt="icone" class="px-6"><a href="#" class="text-white no-underline">Promo Code</a></li>
-                            <li class="p-4 flex"><img src="/Icone/Folder.svg" alt="icone" class="px-6"><a href="#" class="text-white no-underline">Categories</a></li>
-                            <li class="p-4 flex"><img src="/Icone/Controller.svg" alt="icone" class="px-6"><a href="#" class="text-white no-underline">Games</a></li>
+                            <li class="p-4 flex"><img src="/Icone/user.svg" alt="icone" class="px-6"><a href="/User" class="text-white no-underline">User</a></li>
+                            <li class="p-4 flex"><img src="/Icone/Subscription.svg" alt="icone" class="px-6"><a href="/Subscriptions" class="text-white no-underline">Subscriptions</a></li>
+                            <li class="p-4 flex"><img src="/Icone/News.svg" alt="icone" class="px-6"><a href="/Post" class="text-white no-underline">Post</a></li>
+                            <li class="p-4 flex"><img src="/Icone/Subscription.svg" alt="icone" class="px-6"><a href="/Plan" class="text-white no-underline">Plan</a></li>
+                            <li class="p-4 flex"><img src="/Icone/Discount.svg" alt="icone" class="px-6"><a href="/Promo_Code" class="text-white no-underline">Promo Code</a></li>
+                            <li class="p-4 flex"><img src="/Icone/Folder.svg" alt="icone" class="px-6"><a href="/Categories" class="text-white no-underline">Categories</a></li>
+                            <li class="p-4 flex"><img src="/Icone/Controller.svg" alt="icone" class="px-6"><a href="/Games" class="text-white no-underline">Games</a></li>
                         </ul>
                         <div class="flex justify-center">
                             <button class="bg-purple-700 hover:bg-purple-600 text-gray-100 font-semibold py-2 px-4 ">
-                                <a href="#">Log Out</a>
+                                <ResponsiveNavLink :href="route('logout')" method="post" as="button">
+                                    Log Out
+                                </ResponsiveNavLink>
                             </button>
                         </div>
                     </div>
--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,56 @@ Route::middleware('auth', 'admin')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/User')->name('user.index');
+    Route::get('/User/{id}')->name('user.show');
+    Route::post('/User')->name('user.store');
+    Route::put('/User')->name('user.update');
+    Route::delete('/User')->name('user.destroy');
+    
+    
+    Route::get('/Subscriptions')->name('sub.index');
+    Route::get('/Subscriptions/{id}')->name('sub.show');
+    Route::post('/Subscriptions')->name('sub.store');
+    Route::put('/Subscriptions')->name('sub.update');
+    Route::delete('/Subscriptions')->name('sub.destroy');
+
+    
+    Route::get('/Post')->name('post.index');
+    Route::get('/Post/{id}')->name('post.show');
+    Route::post('/Post')->name('post.store');
+    Route::put('/Post')->name('post.update');
+    Route::delete('/Post')->name('post.destroy');
+
+    
+    Route::get('/Plan')->name('plan.index');
+    Route::get('/Plan/{id}')->name('plan.show');
+    Route::post('/Plan')->name('plan.store');
+    Route::put('/Plan')->name('plan.update');
+    Route::delete('/Plan')->name('plan.destroy');
+
+    
+    Route::get('/Promo_Code')->name('promo.index');
+    Route::get('/Promo_Code/{id}')->name('promo.show');
+    Route::post('/Promo_Code')->name('promo.store');
+    Route::put('/Promo_Code')->name('promo.update');
+    Route::delete('/Promo_Code')->name('promo.destroy');
+
+    
+    Route::get('/Categories')->name('category.index');
+    Route::get('/Categories/{id}')->name('category.show');
+    Route::post('/Categories')->name('category.store');
+    Route::put('/Categories')->name('category.update');
+    Route::delete('/Categories')->name('category.destroy');
+
+    
+    Route::get('/Games')->name('game.index');
+    Route::get('/Games/{id}')->name('game.show');
+    Route::post('/Games')->name('game.store');
+    Route::put('/Games')->name('game.update');
+    Route::delete('/Games')->name('game.destroy');
+
+
 });
 
 require __DIR__.'/auth.php';
