5cdc4c8ef3cf888b5dfe6359b58313d39a6cdc9a
Emilie <92803520+SakoSuta@users.noreply.github.com>
2023-06-26 04:14:00 +0200

    ajustation

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,18 +1,12 @@
 <script setup>
 import { ref } from 'vue';
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import Dropdown from '@/Components/Dropdown.vue';
-import DropdownLink from '@/Components/DropdownLink.vue';
-import NavLink from '@/Components/NavLink.vue';
-import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
-import { Link } from '@inertiajs/vue3';
 
 const showingNavigationDropdown = ref(false);
 </script>
 
 <template>
     <div>
-        <div class="min-h-screen bg-gray-100 bg-gray-900 flex">
+        <div class="min-h-screen bg-gray-900 flex">
             
                 <nav class="w-80">
                     <div class="h-full flex flex-col justify-around">
